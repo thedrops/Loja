3bb98aa1bee183f1664504d2b12471e0060d501b
a170060x <a170060x@LAB209M38>
2018-04-04 21:57:34 -0300

    primeiro commit

---
+++ src/loja/bean/Cliente.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loja.bean;
+
+/**
+ *
+ * @author a170060x
+ */
+public class Cliente {
+    int id;
+    String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
